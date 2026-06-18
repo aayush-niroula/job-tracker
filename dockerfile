b2dd4999101a25b51a65929002d69c72bf8bef04
aayush-niroula <ayushniroula644@gmail.com>
2026-06-18 18:45:25 +0545

    feat: update Dockerfile to use npm start, refactor server index for improved static file serving, and add .env.sample for environment configuration

--- dockerfile
+++ dockerfile
@@ -26,4 +26,4 @@ COPY --from=client-builder /app/client/dist /app/client/dist
 
 EXPOSE 3000
 
-CMD ["npm","run","dev"]
+CMD ["npm","start"]
--- server/index.ts
+++ server/index.ts
@@ -4,27 +4,26 @@ import express from "express";
 import cors from "cors";
 import applicationsRouter from "./routes/applications";
 import path from "path";
+import { fileURLToPath } from "url";
+
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 const SERVER_URL = process.env.SERVER_URL ?? `http://localhost:${PORT}`;
 const app = express();
-
-
-const _dirname = path.resolve()
 app.use(cors());
 app.use(express.json());
-
 app.use("/applications", applicationsRouter);
-app.use(
-  express.static(
-    path.join(_dirname, "..", "client", "dist")
-  )
-);
-app.get("/*splat", (req, res) => {
- res.sendFile(
-  path.join(_dirname, "..", "client", "dist", "index.html")
-);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const clientPath = path.join(__dirname, "../client/dist");
+
+app.use(express.static(clientPath));
+
+app.use((req, res) => {
+  res.sendFile(path.join(clientPath, "index.html"));
 });
+
 
 async function bootstrap(): Promise<void> {
   await prisma.$connect();
